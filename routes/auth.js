347bde1be5bd5c91f63ef5f03374b898a2db4a5a
Cristian Daniel Morales Villada <cristian980829@gmail.com>
2021-11-13 15:21:00 -0500

    REGISTER VALIDATE FIELDS

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,22 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { createUser } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validateFields');
 
 
 const router = Router();
 
 router.post(
     '/new',
+    // middlewares
+    [
+        check('name', 'Name is required').not().isEmpty(),
+        check('email', 'Email is required').isEmail(),
+        check('password', 'Password must be 6 characters').isLength({ min: 6 }),
+        check('urlimage', 'Url Image is required').not().isEmpty(),
+        check('rol', 'Rol is required').not().isEmpty(),
+        validateFields
+    ],
     createUser 
 );
 
