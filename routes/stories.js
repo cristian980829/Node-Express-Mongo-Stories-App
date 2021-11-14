0cd62f24227ce7fc801e9f7585ea24a921df3bca
Cristian Daniel Morales Villada <cristian980829@gmail.com>
2021-11-13 19:11:05 -0500

    VALIDATED DATE

--- routes/stories.js
+++ routes/stories.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
+const { isDate } = require('../helpers/isDate');
 const { validateFields } = require('../middlewares/validateFields');
 const { validateJWT } = require('../middlewares/validate-jwt');
 const {  createStorie } = require('../controllers/stories');
@@ -11,13 +12,13 @@ const router = Router();
 router.use( validateJWT );
 
 
-
 // Create a new storie
 router.post(
     '/',
     [
         check('title','Title is required').not().isEmpty(),
         check('description','Description is required').not().isEmpty(),
+        check('registration_date','Registration date invalid').custom( isDate ),
         validateFields
     ],
     createStorie 
